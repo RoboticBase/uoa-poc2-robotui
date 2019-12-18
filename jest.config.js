950d4dcefcc2bdbf36ca5766b503a276a87124ef
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2019-12-18 16:08:27 +0900

    add unit tests for 'views/Initial.vue'

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  setupFilesAfterEnv: ['<rootDir>/tests/unit/jest.setup.js'],
+  globals: {
+    speechSynthesis: null,
+    SpeechSynthesisUtterance: null,
+  },
   collectCoverage: true
 }
---
+++ tests/unit/jest.setup.js
@@ -0,0 +1,6 @@
+/* eslint no-undef: 0, no-global-assign: 0 */
+speechSynthesis = {
+  getVoices: jest.fn().mockReturnValue(Array.from(Array(36), (k, v) => v)),
+  speak: jest.fn(),
+}
+SpeechSynthesisUtterance = jest.fn()
