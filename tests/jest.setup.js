ad1a32db158f2ef54bbcdf97f49342806622725e
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2019-12-27 17:18:26 +0900

    add unit tests for 'speak'

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,6 +1,6 @@
 /* eslint no-undef: 0, no-global-assign: 0 */
 speechSynthesis = {
-  getVoices: jest.fn().mockReturnValue(Array.from(Array(36), (k, v) => v)),
+  getVoices: jest.fn().mockReturnValue(Array.from(Array(37), (k, v) => v)),
   speak: jest.fn(),
 }
 SpeechSynthesisUtterance = jest.fn()
